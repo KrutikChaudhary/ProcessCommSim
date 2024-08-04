c9822a7b324588041e17913d1b0f2d419d65f5e5
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-08-04 09:16:04 -0300

    barrier function

---
+++ prosim/barrier.c
@@ -0,0 +1,33 @@
+//
+// Created by Krutik on 2024-08-04.
+//
+#include "barrier.h"
+#include <stdlib.h>
+extern barrier_t *barrier_new(int n){
+    barrier_t * barr= calloc(1, sizeof(barrier_t));
+    barr->cur_threads=0;
+    barr->max_threads=n;
+    pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_init(&barr->cond, NULL);
+    return barr;
+}
+extern void barrier_wait(barrier_t * curr){
+    pthread_mutex_lock(&curr->lock);
+    curr->cur_threads++;
+    if(curr->cur_threads<curr->max_threads){
+        pthread_cond_wait(&curr->cond, &curr->lock);
+    } else {
+        curr->cur_threads=0;
+    }
+    pthread_cond_broadcast(&curr->cond);//FOR ALL THREADS
+    pthread_mutex_unlock(&curr->lock);
+}
+extern void barrier_done(barrier_t * curr){
+    pthread_mutex_lock(&curr->lock);
+    curr->max_threads--;
+    if (curr->cur_threads == curr->max_threads) {
+        curr->cur_threads = 0;
+        pthread_cond_broadcast(&curr->cond);//FOR ALL THREADS
+    }
+    pthread_mutex_unlock(&curr->lock);
+}
---
+++ prosim/barrier.h
@@ -0,0 +1,18 @@
+//
+// Created by Krutik on 2024-08-04.
+//
+
+#ifndef PROSIM_BARRIER_H
+#define PROSIM_BARRIER_H
+
+#endif //PROSIM_BARRIER_H
+
+typedef struct barrier_t {
+    int max_threads;
+    int cur_threads;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+} barrier_t;
+extern barrier_t *barrier_new(int n);
+extern void barrier_wait(barrier_t * curr);
+extern void barrier_done(barrier_t * curr);
