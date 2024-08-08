b9b6613f9b177f14af4aeeea1198b0564bdb8357
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-08-08 09:33:46 -0300

    use one condition variable

--- prosim/barrier.c
+++ prosim/barrier.c
@@ -8,49 +8,26 @@ extern barrier_t *barrier_new(int n){
     barr->cur_threads=0;
     barr->max_threads=n;
     pthread_mutex_init(&barr->lock, NULL);
-    pthread_cond_init(&barr->cond1, NULL);
-    pthread_cond_init(&barr->cond2, NULL);
-    barr->currCondition=0;
+    pthread_cond_init(&barr->cond, NULL);
     return barr;
 }
 extern void barrier_wait(barrier_t * curr){
     pthread_mutex_lock(&curr->lock);
     curr->cur_threads++;
-    int temp = curr->currCondition;
     if(curr->cur_threads<curr->max_threads){
-        if (temp == 0) {
-            pthread_cond_wait(&curr->cond1, &curr->lock);
-        } else {
-            pthread_cond_wait(&curr->cond2, &curr->lock);
-        }
+        pthread_cond_wait(&curr->cond, &curr->lock);
     } else {
         curr->cur_threads=0;
-        if(curr->currCondition==1){
-            curr->currCondition=0;
-        } else {
-            curr->currCondition=1;
-        }
-        if (temp == 0) {
-            pthread_cond_broadcast(&curr->cond1);
-        } else {
-            pthread_cond_broadcast(&curr->cond2);
-        }
     }
-//    pthread_cond_broadcast(&curr->cond);//FOR ALL THREADS
+    pthread_cond_broadcast(&curr->cond);//FOR ALL THREADS
     pthread_mutex_unlock(&curr->lock);
 }
 extern void barrier_done(barrier_t * curr){
     pthread_mutex_lock(&curr->lock);
     curr->max_threads--;
-    if(curr->currCondition==1){
-        curr->currCondition=0;
-    } else {
-        curr->currCondition=1;
-    }
-    if (curr->currCondition == 0) {
-        pthread_cond_broadcast(&curr->cond1);
-    } else {
-        pthread_cond_broadcast(&curr->cond2);
+    if (curr->cur_threads == curr->max_threads) {
+        curr->cur_threads = 0;
+        pthread_cond_broadcast(&curr->cond);//FOR ALL THREADS
     }
     pthread_mutex_unlock(&curr->lock);
 }
--- prosim/barrier.h
+++ prosim/barrier.h
@@ -11,9 +11,7 @@ typedef struct barrier_t {
     int max_threads;
     int cur_threads;
     pthread_mutex_t lock;
-    pthread_cond_t cond1;
-    pthread_cond_t cond2;
-    int currCondition;
+    pthread_cond_t cond;
 } barrier_t;
 extern barrier_t *barrier_new(int n);
 extern void barrier_wait(barrier_t * curr);
